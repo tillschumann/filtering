5890cca031f91769b583081aade7f3ac129c3015
ewart <timothee.ewart@gmail.com>
2016-09-15 00:09:38 +0200

    clean up

--- main.cpp
+++ main.cpp
@@ -14,12 +14,12 @@ public:
     bloom_filter(uint64_t size = 1024):db(boost::dynamic_bitset<>(1024)){}
 
     inline void insert(const Key& k) {
-        auto hashValues = Hash()(k);
+        uint64_t hashValues = Hash()(k);
         db[hashValues%db.size()] = true;
     }
 
     inline  bool try_contain(const Key& k ) const {
-        auto hashValues = Hash()(k);
+        uint64_t hashValues = Hash()(k);
 
         if (!db[hashValues%db.size()])
             return false;
@@ -28,18 +28,19 @@ public:
     }
 
 private:
-    boost::dynamic_bitset<> db;
+    boost::dynamic_bitset<> db; // the load balancing of neuron is dynamic so no array
 };
 
 int main(){
     bloom_filter<> bf;
 
-    std::vector<uint64_t> v(160384,0); // say 16384 neurons GID
-    std::set<uint64_t> s; // the key=value and no duplication so set
-    std::iota(v.begin(), v.end(), 0);
+    std::vector<uint64_t> v(16384,0); // say 16384 neurons GID
+    std::set<uint64_t> s; // the key=value and no duplication so no multi-set
 
-    //shuffle
+    //shuffle the initial buffer
+    std::iota(v.begin(), v.end(), 0);
     std::random_shuffle(v.begin(),v.end());
+
     //take only 1024
     for(int i=0; i < 1024; ++i){
         bf.insert(v[i]);
@@ -47,14 +48,14 @@ int main(){
     }
 
     //do something to avoid agressive optimization
-    auto sum(0);
+    uint64_t sum(0);
     //timing
     unsigned long long int t1(0),t2(0);
 
     t1 = rdtsc();
     // cneuron checks everything coming from mpi_all_gather
     for(uint64_t i=0; i < v.size(); ++i){
-        if(bf.try_contain(i)){ // I may exist, but not garanty
+        if(bf.try_contain(i)){ // I may exist, but not garantee
             auto search = s.find(i);
             if(search != s.end())
                 sum += *search;
@@ -62,7 +63,7 @@ int main(){
     }
     t2 = rdtsc();
 
-    std::cout << sum << " using prefilter time: " << t2 -t1 << " [cycles] " << std::endl;
+    std::cout << sum << " using bloom_filter time: " << t2 -t1 << " [cycles] " << std::endl;
 
     //reset sum
     sum = 0;
@@ -76,6 +77,5 @@ int main(){
     t2 = rdtsc();
 
     std::cout << sum << " old way time: " << t2 -t1 << " [cycles] " << std::endl;
-
 
 }
