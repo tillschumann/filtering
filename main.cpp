542e05ced0a1f875a6f59b350999c63223b19c7a
ewart <timothee.ewart@gmail.com>
2016-09-15 00:22:16 +0200

    std::bitset

--- main.cpp
+++ main.cpp
@@ -4,21 +4,19 @@
 #include <iostream>
 #include <vector>
 #include <cstdint>
-#include <boost/dynamic_bitset.hpp>
+#include <bitset>
 
 #include "timer_asm.h"
 
-template<class Key = uint64_t, class Hash = std::hash<Key> >
+template<int Size = 1024, class Key = uint64_t, class Hash = std::hash<Key> >
 class bloom_filter{
 public:
-    bloom_filter(uint64_t size = 1024):db(boost::dynamic_bitset<>(1024)){}
-
     inline void insert(const Key& k) {
         uint64_t hashValues = Hash()(k);
         db[hashValues%db.size()] = true;
     }
 
-    inline  bool try_contain(const Key& k ) const {
+    inline  bool may_contain(const Key& k ) const {
         uint64_t hashValues = Hash()(k);
 
         if (!db[hashValues%db.size()])
@@ -28,7 +26,7 @@ public:
     }
 
 private:
-    boost::dynamic_bitset<> db; // the load balancing of neuron is dynamic so no array
+    std::bitset<Size> db; // the load balancing of neuron is dynamic so no array
 };
 
 int main(){
@@ -41,7 +39,7 @@ int main(){
     std::iota(v.begin(), v.end(), 0);
     std::random_shuffle(v.begin(),v.end());
 
-    //take only 1024
+    //take only 1024, both contains but with maybe collision for the filter (here no collision)
     for(int i=0; i < 1024; ++i){
         bf.insert(v[i]);
         s.insert(v[i]);
@@ -55,7 +53,7 @@ int main(){
     t1 = rdtsc();
     // cneuron checks everything coming from mpi_all_gather
     for(uint64_t i=0; i < v.size(); ++i){
-        if(bf.try_contain(i)){ // I may exist, but not garantee
+        if(bf.may_contain(i)){ // I may exist, but not garantee
             auto search = s.find(i);
             if(search != s.end())
                 sum += *search;
