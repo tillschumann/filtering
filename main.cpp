4045e9ddedbd25bf75514c2dd43b6f88b61cd30c
ewart <timothee.ewart@gmail.com>
2016-09-15 00:04:27 +0200

    first version with bench

--- main.cpp
+++ main.cpp
@@ -1,50 +1,81 @@
-#include <array>
+#include <numeric>
+#include <set>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <cstdint>
 #include <boost/dynamic_bitset.hpp>
 
-#include "murmur_hash3.hpp"
+#include "timer_asm.h"
 
-std::array<uint64_t, 2> hash(const uint8_t *data, std::size_t len) {
-    std::array<uint64_t, 2> hashValue;
-    MurmurHash3_x64_128(data, len, 0, hashValue.data());
-    return hashValue;
-}
-
-inline uint64_t nthHash(uint8_t n, uint64_t hashA, uint64_t hashB, uint64_t filterSize) {
-    return (hashA + n * hashB) % filterSize;
-}
-
-//template<class Key, class Hash = std::hash<Key> >
+template<class Key = uint64_t, class Hash = std::hash<Key> >
 class bloom_filter{
 public:
-    bloom_filter(uint64_t size = 1024, uint8_t num = 1):num_hashes(num),db(boost::dynamic_bitset<>(1024)){}
+    bloom_filter(uint64_t size = 1024):db(boost::dynamic_bitset<>(1024)){}
 
-    void add(const uint8_t *data, std::size_t len) {
-        auto hashValues = hash(data, len);
-
-        for (int n = 0; n < num_hashes; n++)
-            db[nthHash(n, hashValues[0], hashValues[1], db.size())] = true;
+    inline void insert(const Key& k) {
+        auto hashValues = Hash()(k);
+        db[hashValues%db.size()] = true;
     }
 
-    bool possiblyContains(const uint8_t *data, std::size_t len) const {
-        auto hashValues = hash(data, len);
+    inline  bool try_contain(const Key& k ) const {
+        auto hashValues = Hash()(k);
 
-        for (int n = 0; n < num_hashes; n++)
-            if (!db[nthHash(n, hashValues[0], hashValues[1], db.size())])
-                return false;
+        if (!db[hashValues%db.size()])
+            return false;
 
         return true;
     }
 
 private:
-    uint8_t num_hashes;
     boost::dynamic_bitset<> db;
 };
 
 int main(){
+    bloom_filter<> bf;
 
+    std::vector<uint64_t> v(160384,0); // say 16384 neurons GID
+    std::set<uint64_t> s; // the key=value and no duplication so set
+    std::iota(v.begin(), v.end(), 0);
 
-    bloom_filter bf;
+    //shuffle
+    std::random_shuffle(v.begin(),v.end());
+    //take only 1024
+    for(int i=0; i < 1024; ++i){
+        bf.insert(v[i]);
+        s.insert(v[i]);
+    }
 
-    
+    //do something to avoid agressive optimization
+    auto sum(0);
+    //timing
+    unsigned long long int t1(0),t2(0);
+
+    t1 = rdtsc();
+    // cneuron checks everything coming from mpi_all_gather
+    for(uint64_t i=0; i < v.size(); ++i){
+        if(bf.try_contain(i)){ // I may exist, but not garanty
+            auto search = s.find(i);
+            if(search != s.end())
+                sum += *search;
+        }
+    }
+    t2 = rdtsc();
+
+    std::cout << sum << " using prefilter time: " << t2 -t1 << " [cycles] " << std::endl;
+
+    //reset sum
+    sum = 0;
+    t1 = rdtsc();
+    // cneuron checks everything coming from mpi_all_gather
+    for(uint64_t i=0; i < v.size(); ++i){
+          auto search = s.find(i);
+          if(search != s.end())
+             sum += *search;
+    }
+    t2 = rdtsc();
+
+    std::cout << sum << " old way time: " << t2 -t1 << " [cycles] " << std::endl;
+
 
 }
